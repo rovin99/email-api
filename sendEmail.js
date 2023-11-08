4e5091706ba9867d160ed990ab766ad6089ce428
Rovin Singh <rovin.singh.it@ghrce.raisoni.net>
2023-11-08 13:27:34 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -31,49 +31,75 @@ app.post('/sendEmail', (req, res) => {
 
   // Create an HTML email template with the JSON data in a table format
   const htmlTemplate = `
-    <html>
-      <head>
-        <style>
-          body {
-            font-family: Arial, sans-serif;
-          }
-          h2 {
-            background-color: #007BFF;
-            color: #fff;
-            padding: 10px;
-          }
-          table {
-            border-collapse: collapse;
-            width: 100%;
-          }
-          th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-          }
-          tr:nth-child(even) {
-            background-color: #f2f2f2;
-          }
-        </style>
-      </head>
-      <body>
-        <h2>Data from Local Server</h2>
-        <table>
-          <tr>
-            <th>Key</th>
-            <th>Value</th>
-          </tr>
-          ${Object.entries(dataToSend)
-            .map(([key, value]) => `
-              <tr>
-                <td>${key}</td>
-                <td>${value}</td>
-              </tr>
-            `)
-            .join('')}
-        </table>
-      </body>
-    </html>
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data from Local Server</title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+      }
+  
+      .container {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+      }
+  
+      h1 {
+        background-color: #007BFF;
+        color: #fff;
+        padding: 10px;
+        text-align: center;
+      }
+  
+      table {
+        width: 100%;
+        border-collapse: collapse;
+        background-color: #fff;
+      }
+  
+      th, td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+      }
+  
+      th {
+        background-color: #007BFF;
+        color: #fff;
+      }
+  
+      tr:nth-child(even) {
+        background-color: #f2f2f2;
+      }
+  
+      @media screen and (max-width: 600px) {
+        table {
+          width: 100%;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Data from Local Server</h1>
+      <table>
+        <tr>
+          <th>Key</th>
+          <th>Value</th>
+        </tr>
+        {{tableRows}}
+      </table>
+    </div>
+  </body>
+  </html>
+  
   `;
 
 
