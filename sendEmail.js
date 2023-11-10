3d669745456cc0f2c97f0d284230921246b79916
Rovin Singh <rovin.singh.it@ghrce.raisoni.net>
2023-11-10 14:03:58 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -26,8 +26,8 @@ const upload = multer({ storage: storage });
 app.post('/sendEmail', upload.array('imageUpload', 5), (req, res) => {
   const dataToSend = req.body;
   const images = req.files;
-  console.log('dataToSend.latestValues:', dataToSend.latestValues);
   const latestValues = JSON.parse(dataToSend.latestValues);
+  console.log(dataToSend.selectedTimes);
   const selectedTimes=JSON.parse(dataToSend.selectedTimes) || [];
   const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -40,14 +40,14 @@ app.post('/sendEmail', upload.array('imageUpload', 5), (req, res) => {
   // Create an HTML email template with the JSON data in a table format
   const htmlTemplate = `
   <!DOCTYPE html>
-  <html>
+  <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Data from Local Server</title>
     <style>
       body {
-        font-family: Arial, sans-serif;
+        font-family: 'Arial', sans-serif;
         background-color: #f4f4f4;
         margin: 0;
         padding: 0;
@@ -57,6 +57,9 @@ app.post('/sendEmail', upload.array('imageUpload', 5), (req, res) => {
         max-width: 600px;
         margin: 0 auto;
         padding: 20px;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+        background-color: #fff;
       }
   
       h1 {
@@ -64,16 +67,18 @@ app.post('/sendEmail', upload.array('imageUpload', 5), (req, res) => {
         color: #fff;
         padding: 10px;
         text-align: center;
+        margin-top: 0;
+        border-radius: 8px 8px 0 0;
       }
   
       table {
         width: 100%;
         border-collapse: collapse;
-        background-color: #fff;
+        margin-top: 15px;
       }
   
       th, td {
-        padding: 8px;
+        padding: 12px;
         text-align: left;
         border-bottom: 1px solid #ddd;
       }
@@ -84,7 +89,7 @@ app.post('/sendEmail', upload.array('imageUpload', 5), (req, res) => {
       }
   
       tr:nth-child(even) {
-        background-color: #f2f2f2;
+        background-color: #f9f9f9;
       }
   
       @media screen and (max-width: 600px) {
@@ -96,12 +101,9 @@ app.post('/sendEmail', upload.array('imageUpload', 5), (req, res) => {
   </head>
   <body>
     <div class="container">
-      <h1>Data from Local Server</h1>
+      <h1>Form Details</h1>
       <table>
-      <tr>
-        <th>Key</th>
-        <th>Value</th>
-      </tr>
+       
         ${Object.entries(latestValues)
           .map(([key, value]) => `
             <tr>
@@ -111,23 +113,27 @@ app.post('/sendEmail', upload.array('imageUpload', 5), (req, res) => {
           `)
           .join('')}
       </table>
-      <table>
-      <tr>
-        <th>Day</th>
-        <th>Time</th>
-      </tr>
-      ${selectedTimes
-        .map(item => `
+  
+      ${selectedTimes.length > 0 ? `
+        <table>
           <tr>
-            <td>${item.day}</td>
-            <td>${item.time}</td>
+            <th>Day</th>
+            <th>Time</th>
           </tr>
-        `)
-        .join('')}
-    </table>
+          ${selectedTimes
+            .map(item => `
+              <tr>
+                <td>${item.day}</td>
+                <td>${item.time}</td>
+              </tr>
+            `)
+            .join('')}
+        </table>
+      ` : ''}
     </div>
   </body>
   </html>
+  
   `;
 
   const mailOptions = {
