4fc5bf1f0a4f41540f38fc08304419c734ac0c20
Rovin Singh <rovin.singh.it@ghrce.raisoni.net>
2023-11-10 13:39:36 +0530

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -39,98 +39,100 @@ app.post('/sendEmail', upload.array('imageUpload', 5), (req, res) => {
   // Create an HTML email template with the JSON data in a table format
   const htmlTemplate = `
   <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Data from Local Server</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #f4f4f4;
-      margin: 0;
-      padding: 0;
-    }
-
-    .container {
-      max-width: 600px;
-      margin: 0 auto;
-      padding: 20px;
-    }
-
-    h1 {
-      background-color: #007BFF;
-      color: #fff;
-      padding: 10px;
-      text-align: center;
-    }
-
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      background-color: #fff;
-    }
-
-    th, td {
-      padding: 8px;
-      text-align: left;
-      border-bottom: 1px solid #ddd;
-    }
-
-    th {
-      background-color: #007BFF;
-      color: #fff;
-    }
-
-    tr:nth-child(even) {
-      background-color: #f2f2f2;
-    }
-
-    @media screen and (max-width: 600px) {
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data from Local Server</title>
+    <style>
+      body {
+        font-family: 'Arial', sans-serif;
+        background-color: #f4f4f4;
+        margin: 0;
+        padding: 0;
+      }
+  
+      .container {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+        background-color: #fff;
+      }
+  
+      h1 {
+        background-color: #007BFF;
+        color: #fff;
+        padding: 10px;
+        text-align: center;
+        margin-top: 0;
+        border-radius: 8px 8px 0 0;
+      }
+  
       table {
         width: 100%;
+        border-collapse: collapse;
+        margin-top: 15px;
       }
-    }
-  </style>
-</head>
-<body>
-  <div class="container">
-    <h1>Data from Local Server</h1>
-    <table>
-      <tr>
-        <th>Key</th>
-        <th>Value</th>
-      </tr>
-      ${Object.entries(latestValues)
-        .map(([key, value]) => `
-          <tr>
-            <td>${key}</td>
-            <td>${value}</td>
-          </tr>
-        `)
-        .join('')}
-    </table>
-
-    ${selectedTimes.length > 0 ? `
+  
+      th, td {
+        padding: 12px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+      }
+  
+      th {
+        background-color: #007BFF;
+        color: #fff;
+      }
+  
+      tr:nth-child(even) {
+        background-color: #f9f9f9;
+      }
+  
+      @media screen and (max-width: 600px) {
+        table {
+          width: 100%;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Form Details</h1>
       <table>
-        <tr>
-          <th>Day</th>
-          <th>Time</th>
-        </tr>
-        ${selectedTimes
-          .map(item => `
+       
+        ${Object.entries(latestValues)
+          .map(([key, value]) => `
             <tr>
-              <td>${item.day}</td>
-              <td>${item.time}</td>
+              <td>${key}</td>
+              <td>${value}</td>
             </tr>
           `)
           .join('')}
       </table>
-    ` : ''}
-  </div>
-</body>
-</html>
-
+  
+      ${selectedTimes.length > 0 ? `
+        <table>
+          <tr>
+            <th>Day</th>
+            <th>Time</th>
+          </tr>
+          ${selectedTimes
+            .map(item => `
+              <tr>
+                <td>${item.day}</td>
+                <td>${item.time}</td>
+              </tr>
+            `)
+            .join('')}
+        </table>
+      ` : ''}
+    </div>
+  </body>
+  </html>
+  
   `;
 
   const mailOptions = {
